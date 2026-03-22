d8d31109307edd323245c2579eda6881369a8c26
BrunaPrestes <bruna.elisa.prestes@gmail.com>
2026-03-22 00:07:46 -0300

    feat: Ex10 wall paint area calculator

--- src/main/java/org/example/passo01/Ex09_CurrencyConverter.java
+++ src/main/java/org/example/passo01/Ex09_CurrencyConverter.java
@@ -8,15 +8,15 @@ import java.util.Scanner;
 void main() {
     Scanner sc = new Scanner(System.in);
     System.out.print("Olá! Bem-vindo(a)!\n 1 dólar = R$5,31\nDigite quantos R$ tem em sua carteira: ");
-    double reais = sc.nextDouble();
+    double real = sc.nextDouble();
 
-    double buy = reais / 5.31;
+    double buy = real / 5.31;
     double taxa = buy * 0.01;
     double value = buy - taxa;
 
     System.out.printf(
             "R$%.2f → U$%.2f%n(taxa de 1%% = $%.2f)%nVocê leva: %.2f dólares",
-            reais, buy, taxa, value);
+            real, buy, taxa, value);
     sc.close();
 
 }
