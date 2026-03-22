d7dd7a297c18693620fce3db3caac940b21d4c86
BrunaPrestes <bruna.elisa.prestes@gmail.com>
2026-03-21 23:49:21 -0300

    feat: Ex09 real to dollar currency converter

--- src/main/java/org/example/passo01/Ex09_CurrencyConverter.java
+++ src/main/java/org/example/passo01/Ex09_CurrencyConverter.java
@@ -1,9 +1,22 @@
-package org.example.passo01;
+/*
+9) Faça um algoritmo que leia quanto dinheiro uma pessoa tem na carteira (em R$)
+e mostre quantos dólares ela pode comprar. Considere US$1,00 = R$3,45.
+ */
 
 import java.util.Scanner;
 
-public class Ex09_CurrencyConverter {
-    public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
-    }
+void main() {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Olá! Bem-vindo(a)!\n 1 dólar = R$5,31\nDigite quantos R$ tem em sua carteira: ");
+    double reais = sc.nextDouble();
+
+    double buy = reais / 5.31;
+    double taxa = buy * 0.01;
+    double value = buy - taxa;
+
+    System.out.printf(
+            "R$%.2f → U$%.2f%n(taxa de 1%% = $%.2f)%nVocê leva: %.2f dólares",
+            reais, buy, taxa, value);
+    sc.close();
+
 }
